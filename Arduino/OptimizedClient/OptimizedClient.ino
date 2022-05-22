9f5614cf9d09cdfa35491576ac402ce78096fc14
ljcmps01 <ljcmps01@gmail.com>
2022-05-21 21:31:58 -0300

    Programa optimizado

--- Arduino/OptimizedClient/OptimizedClient.ino
+++ Arduino/OptimizedClient/OptimizedClient.ino
@@ -1,7 +1,15 @@
+#include <ArduinoJson.h>
 #include "DHT.h"
 
-#define nDHT 2
+#define nDHT 6
 #define DHTTYPE DHT22
+
+#define DHTPIN1 2
+#define DHTPIN2 3
+#define DHTPIN3 5
+#define DHTPIN4 6
+#define DHTPIN5 7
+#define DHTPIN6 8
 
 //Constructores hardcodeados
 /*
@@ -14,15 +22,31 @@ DHT dht6(DHTPIN6, DHTTYPE);
 */
 
 
+//Opciones: 
+//"bajo","bouchard","cuadri","entrepiso","fuente",
+//"principal","ruleta","vip"
+const char* salas[6]={
+  "bajo",
+  "bajo",
+  "bajo",
+  "nueva",
+  "nueva",
+  "vip"
+};
+
+const int id[6]={0,1,2,0,1,0};
+
+
+
 //Constructores de DHT22 optimizados
 DHT dht[nDHT]=
 {
-  {DHTPIN,DHTTYPE},
-  {DHTPIN,DHTTYPE},
-  {DHTPIN,DHTTYPE},
-  {DHTPIN,DHTTYPE},
-  {DHTPIN,DHTTYPE},
-  {DHTPIN,DHTTYPE}
+  {DHTPIN1,DHTTYPE},
+  {DHTPIN2,DHTTYPE},
+  {DHTPIN3,DHTTYPE},
+  {DHTPIN4,DHTTYPE},
+  {DHTPIN5,DHTTYPE},
+  {DHTPIN6,DHTTYPE}
 };
 
 void setup() {
@@ -43,12 +67,39 @@ void setup() {
   }
 }
 
+
 void loop() {
 
+  for(int i=0;i<nDHT;i++)
+  {
+    sendDHT2JsonString(dht[i],i);
+  }
   
-
 }
 
-void sendDHT2MQTT(DHT){
-  float
-}
+char sendDHT2JsonString(DHT sensor,int indice)
+{
+  StaticJsonDocument<100> lectura;
+  char data[100];
+  
+  float temp;
+  float hum;
+
+  lectura["sala"]=salas[indice];
+  lectura["id"]=id[indice];
+
+  temp=sensor.readTemperature();
+  hum=sensor.readHumidity();
+  if(isnan(temp)||isnan(hum))
+  {
+    lectura["temp"]="error";
+    lectura["hum"]="error";
+  }
+  else
+  {
+    lectura["temp"]=temp;
+    lectura["hum"]=hum;    
+  }
+  serializeJson(lectura,data);
+  return data;
+}  
